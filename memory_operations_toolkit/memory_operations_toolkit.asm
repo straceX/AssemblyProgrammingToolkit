4a3778404b299a1ff0ec71f3fb3cbac53f227262
0x80 <netsecdeveloper@gmail.com>
2016-07-28 11:57:50 -0700

    memset function added
    memset function added

--- memory_operations_toolkit/memory_operations_toolkit.asm
+++ memory_operations_toolkit/memory_operations_toolkit.asm
@@ -1,6 +1,6 @@
 [BITS 32]
 SECTION .text
-	global _memcpyASM
+	global _memcpyASM,_memsetASM
 	
 _memcpyASM:
 	push	ebp
@@ -19,3 +19,21 @@ _memcpyASM:
 	pop		esi
 	pop		ebp
 	ret
+	
+_memsetASM:
+	push	ebp
+	mov		ebp, esp
+	push	edi
+	
+	mov		edi, [ebp + 8]
+	mov		al, [ebp + 12]
+	mov		ecx, [ebp + 16]
+	cld
+	rep		stosb
+	
+	mov		eax, [ebp + 8]
+	
+	pop		edi
+	pop		ebp
+	ret
+
--- memory_operations_toolkit/memory_operations_toolkit.h
+++ memory_operations_toolkit/memory_operations_toolkit.h
@@ -1,1 +1,2 @@
 void *memcpyASM(void *dest, const void *source, size_t size);
+void *memsetASM(void *ptr, int ch, size_t n);
